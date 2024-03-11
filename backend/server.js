3555b567a4391e3ce409555c7feb0793e464dec4
CalebeCopello <114925816+CalebeCopello@users.noreply.github.com>
2024-03-11 14:00:29 -0300

    Chat working

--- backend/server.js
+++ backend/server.js
@@ -7,15 +7,20 @@ const app = express()
 const httpServer = createServer(app)
 const io = new Server(httpServer, {
 	cors: {
-		origin: ['http://127.0.0.1:5155', 'http://localhost:5155'],
+		origin: ['http://127.0.0.1:5155', 'http://localhost:5155', 'http://192.168.2.105:5155'],
 	},
 })
 const PORT = process.env.PORT || 3070
 
 app.use(cors())
 
+
 io.on('connection', (socket) => {
     console.log(`User Connected: ${socket.id}`)
+    socket.on('sendMessage', (data) => {
+        socket.emit('receiveMessage', data)
+        socket.broadcast.emit('receiveMessage', data)
+    })
 })
 
 httpServer.listen(PORT, () => {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,40 @@
 import { useState, useEffect } from 'react'
 import io from 'socket.io-client'
 
-const socket = io.connect('http://localhost:3070')
+const socket = io.connect('http://192.168.2.105:3070')
 
 const App = () => {
-  const handleMessage = () => {
-    socket.emit()
-  }
-	return <div>
-  <input type="text" />
-  <button onClick={handleMessage}>message</button>
-  </div>
+	const [message, setMessage] = useState('')
+	const [chat, setChat] = useState([])
+	const handleMessage = () => {
+		const d = new Date()
+		socket.emit('sendMessage', { message: message, id: socket.id, date: d })
+		setMessage('')
+	}
+	useEffect(() => {
+		socket.on('receiveMessage', (data) => {
+			console.log(data)
+			setChat((prev) => [...prev, data.message])
+		})
+		return () => {
+			socket.off('receiveMessage')
+		}
+	}, [socket])
+	return (
+		<div>
+			<input
+				type='text'
+				value={message}
+				onChange={(e) => setMessage(e.target.value)}
+			/>
+			<button onClick={() => handleMessage()}>message</button>
+			<div>
+				{chat.map((message, index) => (
+					<div key={index}>{message}</div>
+				))}
+			</div>
+		</div>
+	)
 }
 
 export default App
